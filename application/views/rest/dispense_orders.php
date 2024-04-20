a71d219bd96ecdc3e86f406420b302a3d7cd5a37
vijay <vijayyadav132200@gmail.com>
2024-04-20 13:52:20 +0530

    disp

--- application/views/rest/dispense_orders.php
+++ application/views/rest/dispense_orders.php
@@ -102,7 +102,7 @@
                                         <div class="row" style="margin-bottom: -16px;">
                                             <div class="col-3">
                                                 <div class="form-group">
-                                                    <label>Counter</label>
+                                                    <label><?= $this->lang->line('counter'); ?></label>
                                                     <select class="form-control form-control-sm" id="kitchen-code" onchange="getTableView1();">  
                                                         <option value=""><?= $this->lang->line('select'); ?></option>
                                                         <?php 
@@ -114,9 +114,7 @@
                                                 </div>
                                             </div>
                                             <div class="col-3">
-                                                <label>
-                                                    Type
-                                                </label>
+                                                <label><?= $this->lang->line('type'); ?></label>
                                                 <select class="form-control form-control-sm" id="dispMode" onchange="getTableView();">  
                                                     <option value="0"><?= $this->lang->line('select'); ?></option> 
                                                     <option value="101"><?= $this->lang->line('thirdParty'); ?></option>
